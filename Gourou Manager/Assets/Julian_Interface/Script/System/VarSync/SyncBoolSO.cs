184fa693dd65d2cc55cf78ec5e5d2d80a568bfc2
Jaworek-Julian <59962629+Jaworek-Julian@users.noreply.github.com>
2021-05-04 17:00:32 +0200

    clean de fichier
    En attente de la partie Exaction / Crise pour rêgler les derniers conflits

--- Gourou Manager/Assets/Julian_Interface/Script/System/VarSync/SyncBoolSO.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSyncBool", menuName = "GourouManager/Ressources/SyncBool")]
-public class SyncBoolSO : SyncVar<bool> {}
