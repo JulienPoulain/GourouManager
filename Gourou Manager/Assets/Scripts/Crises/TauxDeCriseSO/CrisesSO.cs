93836f4806d05c29651b35372d48c5262ebe4dd8
Paletou <59962415+Paletou@users.noreply.github.com>
2021-05-04 16:59:51 +0200

    CrisesV2+ExactionV1

---
+++ Gourou Manager/Assets/Scripts/Crises/TauxDeCriseSO/CrisesSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCrise", menuName = "GourouManager/Crises/Crise")]
+public class CrisesSO : ScriptableObject
+{
+    public float m_tauxDeCrise;
+
+}
