e767abe2702db75f7325fab93cfe0bdbb43590f8
AlienorFache <AlienorFache@users.noreply.github.com>
2021-05-05 13:51:48 +0200

    Institution Update

--- Gourou Manager/Assets/Scripts/Insitution/InstitutionSO.cs
+++ Gourou Manager/Assets/Scripts/Insitution/InstitutionSO.cs
@@ -12,9 +12,13 @@ public class InstitutionSO : ScriptableObject
     [SerializeField] public SyncIntSO m_funds;
     [SerializeField] public SyncIntSO m_members;
     [SerializeField] public SyncIntSO m_fanatics;
+    [SerializeField] public SyncIntSO m_impactOnPop;
+    [SerializeField] public SyncIntSO m_impactOnCrise;
     [SerializeField] public SyncIntSO m_publicExposure;
     [SerializeField] public SyncIntSO m_corruption;
-    [SerializeField] public List<InterlocutorSO> InterlocutorList;
+    [SerializeField] public List<InterlocutorSO> m_interlocutorList;
+    [SerializeField] public List<ExactionSO> m_exactionList;
+    [SerializeField] public List<EventSO> m_eventList;
     //impact ?
     
     enum OpinionOnTheCult
@@ -30,7 +34,7 @@ public class InstitutionSO : ScriptableObject
     {
         List<DialogueSO> DialoguesList = new List<DialogueSO>();
 
-        foreach (InterlocutorSO interlocutor in InterlocutorList)
+        foreach (InterlocutorSO interlocutor in m_interlocutorList)
         {
             if (interlocutor.IsAccessible())
                 DialoguesList.Add(interlocutor.m_dialogue);
