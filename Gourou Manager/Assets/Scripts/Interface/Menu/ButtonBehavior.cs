c3ac5cd774dd08ddb0d5e070d8a6a5fbe9510c4f
Jaworek-Julian <59962629+Jaworek-Julian@users.noreply.github.com>
2021-05-26 20:25:26 +0200

    creation du menu

--- Gourou Manager/Assets/Scripts/Interface/Menu/ButtonBehavior.cs
+++ Gourou Manager/Assets/Scripts/Interface/Menu/ButtonBehavior.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonBehavior : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
+public class ButtonBehavior : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerEnterHandler
 {
-    [SerializeField] private float m_scaleDuration = 1f;
+    // en static parce qu'il s'agit une valeur qui sera commun à tout le monde
+    [SerializeField] private float m_scaleDuration;
+    [SerializeField] private string m_SceneName;
     private Image m_image;
     
     void Start()
@@ -17,7 +19,8 @@ public class ButtonBehavior : MonoBehaviour, IPointerDownHandler, IPointerExitHa
     public void OnPointerDown(PointerEventData eventData)
     {
         m_image.sprite = MenuManager.Instance.RedSprite;
-        StartCoroutine(ScaleChange());
+        MenuManager.Instance.LoadScene(m_SceneName);
+        Debug.Log("je clicke dessus");
     }
     
     public void OnPointerExit(PointerEventData eventData)
@@ -26,25 +29,33 @@ public class ButtonBehavior : MonoBehaviour, IPointerDownHandler, IPointerExitHa
         StartCoroutine(ReverseScaleChange());
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(ScaleChange());
+        Debug.Log("je suis dessus");
+    }
+
     IEnumerator ScaleChange()
     {
-        float time = 0;
+        float time = 1f;
         while (time < m_scaleDuration)
         {
-            transform.localScale += Vector3.one * Mathf.Sin(4f) * Time.deltaTime;
-            yield return null;
+            transform.localScale += Vector3.one * Mathf.Log(time) * Time.deltaTime * 3;
             time += Time.deltaTime;
+            Debug.Log("JE RENTRE DEDANS" + time);
+            yield return null;
         }
     }
     
     IEnumerator ReverseScaleChange()
     {
-        float time = 0;
+        float time = 1f;
         while (time < m_scaleDuration)
         {
-            transform.localScale -= Vector3.one * Mathf.Sin(2f) * Time.deltaTime;
-            yield return null;
             time += Time.deltaTime;
+            transform.localScale -= Vector3.one * Mathf.Log(time) * Time.deltaTime * 3;
+            yield return null;
+            Debug.Log("JE SORS DE LA" + time);
         }
     }
 }
