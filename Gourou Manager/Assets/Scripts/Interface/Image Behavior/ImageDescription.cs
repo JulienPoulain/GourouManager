f87926278f2cf80d0fddff11fc95141d6f1f269b
Paletou <59962415+Paletou@users.noreply.github.com>
2021-06-02 11:39:10 +0200

    Descritpion
    Ajout des descriptions des ressources

--- Gourou Manager/Assets/Scripts/Interface/Image Behavior/ImageDescription.cs
+++ Gourou Manager/Assets/Scripts/Interface/Image Behavior/ImageDescription.cs
@@ -10,7 +10,7 @@ public class ImageDescription : MonoBehaviour, IPointerExitHandler, IPointerEnte
     [SerializeField] RessourceType m_ressourceName;
     [SerializeField] TMP_Text m_textPrefab;
 
-    // Stocke le GO instancier pour le d�truire
+    // Stocke le GO instancier pour le d�truire
     TMP_Text m_actualPrefab;
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -19,22 +19,23 @@ public class ImageDescription : MonoBehaviour, IPointerExitHandler, IPointerEnte
         switch (m_ressourceName)
         {
             case RessourceType.Founds:
-                message = "Fonts";
+                message = "Fonds : Les fonds permettent de réaliser certaines éxactions telles que soudoyer. " +
+                          "Chaque institution possède ses propres fonds qui leur permettent de lancer des évènements pour ou contre le culte";
                 break;
             case RessourceType.Membres:
-                message = "Membres";
+                message = "Membres : Les membres sont votre condition de victoire, obtenez en 100 pour gagner la partie. Votre impact sur la population ainsi que celle des institutions font varier leur nombre ";
                 break;
             case RessourceType.Fanatiques:
-                message = "Fanatiques";
+                message = "Points de fanatisme : Les points de fanatisme sont utilisés pour lancer certaines éxactions, ils ont beaucoup de valeur et sont relativement difficile à obtenir ";
                 break;
             case RessourceType.Tour:
-                message = "Jour";
+                message = "Jour : ";
                 break;
             case RessourceType.ExpositionPublic:
-                message = "Exposition Public";
+                message = "Exposition Publique : L'exposition publique représente la façon dont l'institution est perçue par la population, plus elle est élevée et plus l'institution est connue et bien vue, elle est également souvent requise pour accèder a certain interlocuteurs";
                 break;
             case RessourceType.Corruption:
-                message = "Corruption";
+                message = "Corruption : La corruption d'une institution représente sa dévotion au culte et impact directement le statut de l'institution à son égard, plus elle est élevée plus l'institution sera amicale  ";
                 break;
         }
 
