d950b3745c94b7bbd3b2021155390fb625522547
AlienorFache <AlienorFache@users.noreply.github.com>
2021-06-04 01:38:26 +0200

    maj shader corruption

--- Gourou Manager/Assets/Shader/CorruptionVisualTransition.cs
+++ Gourou Manager/Assets/Shader/CorruptionVisualTransition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,30 +8,37 @@ public class CorruptionVisualTransition : MonoBehaviour
     private MaterialPropertyBlock m_mpb;
     private float m_corruptionLevel;
     [SerializeField] private Renderer m_renderer;
-    [SerializeField] private InstitutionSO m_institution;
+    private DecaySO m_decay;
     
-    void OnEnable()
+    private void Awake()
     {
-        if (m_institution == null) Debug.Log($"<color=red>ERROR :</color> {m_renderer.name} institution manquante dans CVT.");
-        else
-        {
-            m_corruptionLevel = 0;
-            m_mpb = new MaterialPropertyBlock();
-            m_mpb.SetFloat ("_corruptionLevel", m_corruptionLevel);
-            m_renderer.SetPropertyBlock (m_mpb);
-        }
+        m_decay = gameObject.GetComponentInParent<InstitutionScript>().m_Institution.Decay;
+        m_mpb = new MaterialPropertyBlock();
     }
 
-    // Update is called once per frame
-    public void Update()
+    void OnEnable()
     {
-        if (m_institution != null)
-        {
-            //calcul : (decay/(range de decay/range de _corruptionLevel)) - offset de la corruption
-            //A modifier si les range et offset sont modifiés
-            m_corruptionLevel = (m_institution.Decay.Value / 50) - 2;
-            m_mpb.SetFloat("_corruptionLevel", m_corruptionLevel);
-            m_renderer.SetPropertyBlock(m_mpb);
-        }
+        m_decay.onValueChanged += HandleDecayChanged;
+        
+        Conversion();
+    }
+
+    private void OnDisable()
+    {
+        m_decay.onValueChanged -= HandleDecayChanged;
+    }
+
+    private void Conversion()
+    {
+        //calcul : (decay/(range de decay/range de _corruptionLevel)) - offset de la corruption
+        //A modifier si les range et offset sont modifiés
+        m_corruptionLevel = m_decay.Value / 62.5f - 1.6f;
+        m_mpb.SetFloat("_corruptionLevel", m_corruptionLevel);
+        m_renderer.SetPropertyBlock(m_mpb);
+    }
+    
+    private void HandleDecayChanged(int p_value)
+    {
+        Conversion();
     }
 }
